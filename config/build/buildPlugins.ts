179ac3122987ea6e60565668602d7ce2273ec8c9
chewmountain <mironov.develop@gmail.com>
2023-06-28 14:01:03 +0300

    2023-06-28

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins({
     paths,
@@ -19,5 +20,10 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
+        /**
+         * Анализирует размер бандла
+         * { openAnalyzer: false }
+         */
+        new BundleAnalyzerPlugin(),
     ];
 }
