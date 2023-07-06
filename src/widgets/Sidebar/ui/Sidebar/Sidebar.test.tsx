8eaa5ec7c0b093a3dab6e519185ecdb746c1ed64
chewmountain <mironov.develop@gmail.com>
2023-07-06 20:16:46 +0300

    2023-07-06 Сайдбар. Состояния кнопки. UI Screenshot test report <-- репорты на скриншотные тесты не делал, т.к. сторибук 7 не дружит с локи старой версии

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,17 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { Sidebar } from "./Sidebar";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
 
 describe("Sidebar react component", () => {
     test("Sidebar", () => {
         // @ts-ignore
-        render(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     });
 
     test("Sidebar on toggle", () => {
         // @ts-ignore
-        render(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId("sidebar-toggle");
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
         fireEvent.click(toggleBtn);
